eadeb4cfadfa9aed4c144845812082c184123c9e
Martynas <xmartissx@gmail.com>
2023-12-20 17:57:27 +0200

    http

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from flask import Flask, jsonify, redirect, url_for
 from flask_dance.contrib.google import make_google_blueprint, google
-from flask_talisman import Talisman
 import os
 
 app = Flask(__name__)
-Talisman(app)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.getenv("google_client_id")
 app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.getenv("google_client_secret")
+app.config["OAUTHLIB_INSECURE_TRANSPORT"] = 1 #HTTP INSTEAD OF HTTPS !!!!!!!!!!!!!!!!!!!!!!!
 
 google_bp = make_google_blueprint(scope=["profile", "email"])
 app.register_blueprint(google_bp, url_prefix="/login")
