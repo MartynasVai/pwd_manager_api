6d48601ed7f5bc9b822dd5afba358faa95682624
Martynas <45741119+MartynasVai@users.noreply.github.com>
2023-12-20 16:57:18 +0200

    Update main.py
    google authentication

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
 import os
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.getenv("google_client_id")
+app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.getenv("google_client_secret")
+
+google_bp = make_google_blueprint(scope=["profile", "email"])
+app.register_blueprint(google_bp, url_prefix="/login")
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v1/userinfo")
+    return jsonify(resp.json())
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
