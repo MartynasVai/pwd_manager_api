b0c369bf2eb64c8c3108fab2f5b58b5cfb56a4f4
Martynas <xmartissx@gmail.com>
2023-12-27 14:22:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 from flask import Flask, jsonify, redirect, url_for
-from flask_dance.contrib.google import make_google_blueprint, google
 import os
 
-# Set the OAUTHLIB_INSECURE_TRANSPORT environment variable
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.getenv("google_client_id")
-app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.getenv("google_client_secret")
 
-google_bp = make_google_blueprint(scope=["openid","https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"])
-app.register_blueprint(google_bp, url_prefix="/login")
 
 @app.route('/')
 def index():
-    if not google.authorized:
-        return "NOT LOGGED IN"
-    else:
-        return "Logged in"
+
+    return jsonify("NOT LOGGED IN")
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
